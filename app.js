f4444932914ed018595b6559fed1c03d6d6fb3c7
Andrew ONeill <andy@evolve757.com>
2026-02-16 12:25:28 -0500

    edited and completed all exercises.

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ function checkAge (name, age){
     }else{
         console.log(underSixteen);
     }
-}
+} 
+checkAge("Andy", 44);
+checkAge("Julie", 10);
+
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 function quadrant(x, y){
@@ -38,20 +41,22 @@ function quadrant(x, y){
         console.log("Quadrant Four");
     }
     else if (x == 0 && y != 0){
-        console.log("Axis X")
+        console.log("Axis Y")
     }
     else if (x != 0 && y == 0){
-        console.log("Axis Y")
+        console.log("Axis X")
     }else {
         console.log("Not in a Quadrant");
     }
 }
+quadrant(5, 8);
+quadrant(0, 5);
 
 //Exercise 4
 console.log("EXERCISE 4:\n==========\n");
 function typeOfTriangle(sideOne, sideTwo, sideThree){
     let triangleType = "";
-    if (sideOne + sideTwo < sideThree || sideOne + sideThree < sideTwo || sideTwo + sideThree < sideOne){
+    if (sideOne + sideTwo <= sideThree || sideOne + sideThree <= sideTwo || sideTwo + sideThree <= sideOne){
         triangleType = "Invalid Triangle";
     }
     else if (sideOne == sideTwo && sideTwo == sideThree){
@@ -63,34 +68,43 @@ function typeOfTriangle(sideOne, sideTwo, sideThree){
     else{
         triangleType = "Scalene Triangle";
     }
-    console.log("These sides form a {triangleType}");
+    console.log(`These sides form a ${triangleType}`);
 }
+typeOfTriangle(5, 8, 7);
+typeOfTriangle(5, 5, 5)
+typeOfTriangle(1, 1, 4)
 
 //Exercise 5
 console.log("EXERCISE 5:\n==========\n");
 function dataUsageStatus(planLimit, day, usage){
-    let periodLength = 30;
-    let averageDailyUse = usage / day;
-    let averageDailyAllowedPace = planLimit / periodLength;
-    let remainingDays = periodLength - day;
-    let usagePace = averageDailyUse * remainingDays;
-    let statusUpdate;
-    let remainingData = planLimit - usage;
-    
-    console.log(`${day} days used, ${remainingDays} days remaining`);
-    console.log(`Average Daily Use: ${averageDailyUse}`);
-    
-    if(averageDailyUse > averageDailyAllowedPace){
-        statusUpdate = "Exceeding";
+    const periodLength = 30;
+    if(usage >= planLimit){
+        console.log("You have used all of your data");
+        return;
     }
-    else if(averageDailyUse < averageDailyAllowedPace){
-        statusUpdate = "Under";
+    
+    const remainingDays = periodLength - day;
+    console.log(`${day} days used, ${remainingDays} days remaining`);
+    
+    const averageDailyUse = usage / day;
+    console.log(`Average Daily Use: ${averageDailyUse.toFixed(2)}`);
+    
+    const averageDailyAllowedPace = planLimit / periodLength;
+    
+    const usagePace = averageDailyUse * periodLength;
+    const overBy = usagePace - planLimit;
+    const recommendedDailyUse = (planLimit - usage) / remainingDays;
+    
+    const remainingData = planLimit - usage;
+    
+    let statusUpdate;
+    if(averageDailyUse > averageDailyAllowedPace){
+        statusUpdate = `You are exceeding your ${averageDailyUse.toFixed(2)} by GB/day. If you continue at this pace you'll exceed your plan by ${overBy.toFixed(2)}. To stay below your usage plan, you will need to continue at ${recommendedDailyUse.toFixed(2)}`;
     }
     else {
-        statusUpdate = "At";
+        statusUpdate = `You are on track to stay below or equal to your plan. You have ${remainingData.toFixed(2)} GB left for the rest of your period. You can use up to ${recommendedDailyUse.toFixed(2)} GB/day.`;
     }
     
-    console.log(`You are ${statusUpdate} your average daily use (${averageDailyUse}), continuing this usage
-    you'll end up using ${planLimit - usage + usagePace} of your plan. To stay below your usage plan, use no more than
-    ${remainingData / remainingDays} GB per day.`) 
+    console.log(statusUpdate);
 }
+dataUsageStatus(900, 14, 400);
